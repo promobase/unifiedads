78e0f828f76b7a20357645ec3dde559314a277e7
Ruize Li <rl3250@columbia.edu>
2025-07-14 15:04:08 -0700

    sync

--- src/main/java/com/promobase/unifiedads/UnifiedadsApplication.java
+++ src/main/java/com/promobase/unifiedads/UnifiedadsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnifiedadsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnifiedadsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(UnifiedadsApplication.class, args);
+  }
 
 }
--- src/test/java/com/promobase/unifiedads/UnifiedadsApplicationTests.java
+++ src/test/java/com/promobase/unifiedads/UnifiedadsApplicationTests.java
@@ -1,13 +1,8 @@
 package com.promobase.unifiedads;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UnifiedadsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
